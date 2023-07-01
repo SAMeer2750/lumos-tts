35e5cbaa4809832d4802f2d6a5f92865c8753806
Sameer Madhav D <sameermadhav.5@gmail.com>
2023-07-01 22:52:21 +0530

    play buttom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
+import Polygon from './components/Polygon 1.svg'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="content">
-        <textarea 
-        className="textBox"
-        />
+        <textarea className="textBox" />
+        <div className="playButton">
+          <img src={Polygon} alt="" className='play'/>
+        </div>
       </div>
     </div>
   );
